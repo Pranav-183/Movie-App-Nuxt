4aec93417c9d9782068e6809919ea636598a7f9c
Pranav <brindhameera@gmail.com>
2022-09-01 12:24:42 +0530

    Added Project Files

---
+++ _nuxt/static/1662014826/movies/438148/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMovie-App-Nuxt\u002F_nuxt\u002Fstatic\u002F1662014826",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F438148",config:{_app:{basePath:"\u002FMovie-App-Nuxt\u002F",assetsPath:"\u002FMovie-App-Nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
